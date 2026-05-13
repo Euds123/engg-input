2ae496a3d6ea1f15501778d5b6702029ce4a9bf7
Euds123 <faizan.shaikh@epcproman.com>
2026-05-13 19:11:35 +0530

    Add files via upload

--- schema.sql
+++ schema.sql
@@ -1,59 +1,41 @@
-/* 
-   SQL Script for Creating the Component Details Database and Table in SSMS 
-*/
+-- PostgreSQL / Neon: create table for Component Details
+-- Run this in the Neon SQL Editor (or psql) against your database before using the app.
 
--- Create the Database
-IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'ComponentDB')
-BEGIN
-    CREATE DATABASE ComponentDB;
-END
-GO
+CREATE TABLE IF NOT EXISTS "ComponentDetails" (
+    "Id" SERIAL PRIMARY KEY,
+    "DrawingId" VARCHAR(100),
+    "LineId" VARCHAR(100),
+    "RevNo" VARCHAR(50),
+    "SpoolNo" VARCHAR(100),
+    "Item" VARCHAR(100),
+    "ItemCode" VARCHAR(100),
+    "Description" TEXT,
+    "Size_Inch" NUMERIC(18, 4),
+    "MFA" VARCHAR(100),
+    "SMIV" VARCHAR(100),
+    "HMIV" VARCHAR(100),
+    "SubContractor" VARCHAR(100),
+    "IsMIVLinesIssuance" BOOLEAN,
+    "ComponentStatus" VARCHAR(100),
+    "InsuType" VARCHAR(100),
+    "InsuThickness" NUMERIC(18, 2),
+    "InsuLength" NUMERIC(18, 2),
+    "RussianDescription" TEXT,
+    "Specification" VARCHAR(100),
+    "Length_InchMeter" NUMERIC(18, 4),
+    "ComponentWeight" NUMERIC(18, 4),
+    "ComponentSurfaceArea" NUMERIC(18, 4),
+    "PaintSystem" VARCHAR(100),
+    "UniqueComponentIdentifier" VARCHAR(100),
+    "ErectionDrawingNo" VARCHAR(100),
+    "UId" UUID DEFAULT gen_random_uuid(),
+    "Quantity" NUMERIC(18, 4),
+    "Length" NUMERIC(18, 4),
+    "Part_No" VARCHAR(100),
+    "Description_Language" VARCHAR(100),
+    "SheetNo" VARCHAR(50),
+    "CreatedAt" TIMESTAMPTZ DEFAULT NOW()
+);
 
-USE ComponentDB;
-GO
-
--- Create the ComponentDetails Table
-IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ComponentDetails]') AND type in (N'U'))
-BEGIN
-    CREATE TABLE [dbo].[ComponentDetails] (
-        [Id] [int] IDENTITY(1,1) PRIMARY KEY,
-        [DrawingId] [nvarchar](100) NULL,
-        [LineId] [nvarchar](100) NULL,
-        [RevNo] [nvarchar](50) NULL,
-        [SpoolNo] [nvarchar](100) NULL,
-        [Item] [nvarchar](100) NULL,
-        [ItemCode] [nvarchar](100) NULL,
-        [Description] [nvarchar](MAX) NULL,
-        [Size_Inch] [decimal](18, 4) NULL,
-        [MFA] [nvarchar](100) NULL,
-        [SMIV] [nvarchar](100) NULL,
-        [HMIV] [nvarchar](100) NULL,
-        [SubContractor] [nvarchar](100) NULL,
-        [IsMIVLinesIssuance] [bit] NULL,
-        [ComponentStatus] [nvarchar](100) NULL,
-        [InsuType] [nvarchar](100) NULL,
-        [InsuThickness] [decimal](18, 2) NULL,
-        [InsuLength] [decimal](18, 2) NULL,
-        [RussianDescription] [nvarchar](MAX) NULL,
-        [Specification] [nvarchar](100) NULL,
-        [Length_InchMeter] [decimal](18, 4) NULL,
-        [ComponentWeight] [decimal](18, 4) NULL,
-        [ComponentSurfaceArea] [decimal](18, 4) NULL,
-        [PaintSystem] [nvarchar](100) NULL,
-        [UniqueComponentIdentifier] [nvarchar](100) NULL,
-        [ErectionDrawingNo] [nvarchar](100) NULL,
-        [UId] [uniqueidentifier] DEFAULT NEWID(),
-        [Quantity] [decimal](18, 4) NULL,
-        [Length] [decimal](18, 4) NULL,
-        [Part_No] [nvarchar](100) NULL,
-        [Description_Language] [nvarchar](100) NULL,
-        [SheetNo] [nvarchar](50) NULL,
-        [CreatedAt] [datetime] DEFAULT GETDATE()
-    );
-END
-GO
-
--- Create Index for common search fields
-CREATE INDEX IX_DrawingId ON [dbo].[ComponentDetails] (DrawingId);
-CREATE INDEX IX_LineId ON [dbo].[ComponentDetails] (LineId);
-GO
+CREATE INDEX IF NOT EXISTS "IX_DrawingId" ON "ComponentDetails" ("DrawingId");
+CREATE INDEX IF NOT EXISTS "IX_LineId" ON "ComponentDetails" ("LineId");
